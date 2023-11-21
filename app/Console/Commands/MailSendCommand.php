9c43843da09e78745b983d940cd40a399eea3c71
Aram <papoyanaram2@gmail.com>
2023-11-21 17:08:45 +0400

    change functionality send email

--- app/Console/Commands/MailSendCommand.php
+++ app/Console/Commands/MailSendCommand.php
@@ -25,41 +25,50 @@ class MailSendCommand extends Command
      */
     protected $description = 'Command description';
 
+    protected function sendEmailAndRecordPost($website, $post, $user): void
+    {
+        $data = [
+            'web_site_title' => $website->title,
+            'post_title' => $post->title,
+            'email' => $user->email,
+        ];
+
+        $email = new SendEmailTest($data);
+        SendEmailJob::dispatch($email);
+
+        $subscriberPostData = [
+            'email' => $user->email,
+            'post_id' => $post->id,
+        ];
+        SubscriberPost::query()->create($subscriberPostData);
+    }
+
     /**
      * Execute the console command.
      */
     public function handle(): void
     {
-        // Retrieve the timestamp of the latest post creation.
-        $createdAt = SubscriberPost::query()->orderBy('created_at', 'DESC')->pluck('created_at')->first();
+        try {
+            // Retrieve the timestamp of the latest post creation.
+            $createdAt = SubscriberPost::query()->orderBy('created_at', 'DESC')->pluck('created_at')->first();
+            // Check if there's a timestamp, then fetch posts accordingly.
+            if (isset($createdAt)) {
+                $posts = Post::query()->where('created_at', '>=', $createdAt)->get();
+            } else {
+                $posts = Post::query()->get();
+            }
 
-        // Check if there's a timestamp, then fetch posts accordingly.
-        if (isset($createdAt)) {
-            $posts = Post::query()->where('created_at', '>=', $createdAt)->get();
-        } else {
-            $posts = Post::query()->get();
-        }
-
-        foreach ($posts as $post) {
-            $website = $post->website;
-            $website->users()->chunk(10, function ($users) use ($website, $post) {
-                foreach ($users as $user) {
-                    $data = [
-                        'web_site_title' => $website->title,
-                        'post_title' => $post->title,
-                        'email' => $user->email
-                    ];
-
-                    $email = new SendEmailTest($data);
-                    SendEmailJob::dispatch($email);
-
-                    $subscriberPostData = [
-                        'email' => $user->email,
-                        'post_id' => $post->id
-                    ];
-                    SubscriberPost::query()->create($subscriberPostData);
-                }
-            });
+            foreach ($posts as $post) {
+                $website = $post->website;
+                $website->users()->chunk(10, function ($users) use ($website, $post) {
+                    foreach ($users as $user) {
+                        $this->sendEmailAndRecordPost($website, $post, $user);
+                    }
+                });
+            }
+        } catch (\Exception $e) {
+            $this->error("An error occurred: {$e->getMessage()}");
+            // Log the error or take appropriate action.
         }
     }
 }
